835efb74d573a041d5ba20eb30c6818ba355fc43
issei4369 <41623959+issei4369@users.noreply.github.com>
2021-12-05 13:49:14 -0600

    added portal changes, added the start screen

--- Player.gd
+++ Player.gd
@@ -33,6 +33,7 @@ const graveyard_level = [
   ]
 
 const castle_level = [
+  
   { 'StartPosition' : Vector2(  160, 320 ), 'CameraLimits' : [  0, 1280, 0, 800 ] },# room1
   { 'StartPosition' : Vector2( 1600, 400 ), 'CameraLimits' : [ 1440, 2720, 0, 800 ] },# room2
   { 'StartPosition' : Vector2( 1120, 400 ), 'CameraLimits' : [ 0, 1280, 0, 800 ]}, #room1
@@ -43,6 +44,17 @@ const castle_level = [
   { 'StartPosition' : Vector2( 2080, -1280 ), 'CameraLimits' : [ 1440, 2720, -1920, -1120 ]},#room 4
   { 'StartPosition' : Vector2( 2080, -800 ), 'CameraLimits' : [ 1440, 2720, -960, -160 ]} #room 3
   ]
+  
+const openfield_level = [
+  { 'StartPosition' : Vector2(  840, 600), 'CameraLimits' : [  0, 1280, 0, 800 ] },# room1
+  { 'StartPosition' : Vector2( 640, -320 ), 'CameraLimits' : [ 0, 1280, -960, -160 ] },# room2
+  { 'StartPosition' : Vector2( 640, 160 ), 'CameraLimits' : [ 0, 1280, 0, 800 ]}, #room1
+  { 'StartPosition' : Vector2( 1600, -560 ), 'CameraLimits' : [ 1440, 2648, -960, -160 ]},#room3
+  { 'StartPosition' : Vector2( 1120, -560 ), 'CameraLimits' : [ 0, 1280, -960, -160 ]},#room 2
+  { 'StartPosition' : Vector2( -320, -560 ), 'CameraLimits' : [ -2640, -160, -960, 1200 ]},# boss room
+  { 'StartPosition' : Vector2( 160, -560 ), 'CameraLimits' : [ 0, 1280, -960, -160 ]}#room 2
+  
+ ]
 
 var currentLevel : = 0
 
@@ -233,6 +245,20 @@ func gotoLevel( which : int = -1 ) -> void :
     $Camera2D.limit_right = castle_level[which][ 'CameraLimits' ][1]
     $Camera2D.limit_top = castle_level[which][ 'CameraLimits' ][2]
     $Camera2D.limit_bottom = castle_level[which][ 'CameraLimits' ][3]
+    
+  elif in_field == true:
+    if which < 0 :
+      which = currentLevel
+
+    if which >= openfield_level.size() :
+      print( "Finished last level, so going back to the beginning." )
+      which = 0
+
+    position = openfield_level[which][ 'StartPosition' ]
+    $Camera2D.limit_left  = openfield_level[which][ 'CameraLimits' ][0]
+    $Camera2D.limit_right = openfield_level[which][ 'CameraLimits' ][1]
+    $Camera2D.limit_top = openfield_level[which][ 'CameraLimits' ][2]
+    $Camera2D.limit_bottom = openfield_level[which][ 'CameraLimits' ][3]
   
 func increase_dmg():
   arrow_dmg += 10
--- Portal.gd
+++ Portal.gd
@@ -6,23 +6,23 @@ export var whichLevel : = 0
 export var next_scene: PackedScene
 
 func _on_Portal_body_entered( body : PhysicsBody2D ) -> void :
-  if ( whichLevel == 2|| whichLevel == 5 || whichLevel == 7):
-    print("Player entered room 3")
-    
-    body.position = Vector2(2080, -320)
-    print(body.position) 
-  elif (whichLevel == 1):
-    print("player entered room 1")
-  elif (whichLevel == 0 || whichLevel == 3):
-    print(" Player entered room 2")
-  elif (whichLevel == 6):
-    print("player entered room 4")
-  elif ( whichLevel == 4):
-    print("player entered boss room")
-  print("player position before portal ", body.get_global_position())  
+#  if ( whichLevel == 2|| whichLevel == 5 || whichLevel == 7):
+#    print("Player entered room 3")
+#
+#    body.position = Vector2(2080, -320)
+#    print(body.position) 
+#  elif (whichLevel == 1):
+#    print("player entered room 1")
+#  elif (whichLevel == 0 || whichLevel == 3):
+#    print(" Player entered room 2")
+#  elif (whichLevel == 6):
+#    print("player entered room 4")
+#  elif ( whichLevel == 4):
+#    print("player entered boss room")
+#  print("player position before portal ", body.get_global_position())  
 
   body.gotoLevel( whichLevel + 1 )
-  
+#
   print("player position after portal ", body.get_global_position())
 
 func _on_body_entered(body: PhysicsBody2D):
@@ -30,11 +30,7 @@ func _on_body_entered(body: PhysicsBody2D):
   
 func teleport() -> void:
   get_tree().change_scene_to(next_scene)
-  if get_tree().root.get_node("Graveyard_Level").get_node("Player").in_graveyard == true:
-    
-    print("look at here ",get_tree().root.get_node("Graveyard_Level").get_node("Player"))
-    get_tree().root.get_node("Castle_Level").get_node("Player").in_graveyard = false
-    get_tree().root.get_node("Castle_Level").get_node("Player").in_castle = true
+  print("I changed scenes")
     
   
 
