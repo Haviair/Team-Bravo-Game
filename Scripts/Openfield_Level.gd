4cb1b00d5c1f7fe54ffe689a131301f3edd84d88
Nihal Kumarswamy <33848011+KumarswamyNihal@users.noreply.github.com>
2021-12-08 12:05:16 -0600

    adding music for openfield

--- Scripts/Openfield_Level.gd
+++ Scripts/Openfield_Level.gd
@@ -6,3 +6,5 @@ func _ready() -> void:
     get_tree().root.get_node("Openfield_Level/Player").in_graveyard = false
     get_tree().root.get_node("Openfield_Level/Player").in_castle = false
     get_tree().root.get_node("Openfield_Level/Player").cur_lvl = "Openfield_Level"
+    if $AudioStreamPlayer.playing == false :
+      $AudioStreamPlayer.play()
