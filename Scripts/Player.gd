f9a5264404bb8cc1466b9e6d2382f55c4967d951
Nihal Kumarswamy <33848011+KumarswamyNihal@users.noreply.github.com>
2021-12-08 10:55:34 -0600

    fixed bug: Player hp changes depends on level

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -202,17 +202,18 @@ func _enemy_body_entered(body: Node):
     hp -= 10
     $AudioStreamPlayer.stream = preload("res://Music/player_grunt.wav")
     $AudioStreamPlayer.play()
-    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/HealthBar").value = hp
+    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/Healthbar").value = hp
     damagetimer.start()
   elif 'Boss' in body.get_name():
     hp -= 10
-    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/HealthBar").value = hp
+    
+    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/Healthbar").value = hp
     en_dmg_timer.start()
   elif 'Orb' in body.get_name() or 'projectile' in body.get_name():
     body.remove()
     hp -= 25
     print(cur_lvl)
-    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/HealthBar").value = hp
+    get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/Healthbar").value = hp
     
 func _enemy_body_exited(body: Node):
   if 'Enemy' in body.get_name():
@@ -224,7 +225,7 @@ func _on_damage_timer_timeout():
   hp -= 10
   $AudioStreamPlayer.stream = preload("res://Music/player_grunt.wav")
   $AudioStreamPlayer.play()
-  get_tree().root.get_node("Graveyard_Level").get_node("CanvasLayer2").get_node("Player_UI").get_node("Healthbar").value = hp
+  get_tree().root.get_node(cur_lvl+"/CanvasLayer2/Player_UI/Healthbar").value = hp
 
 func gotoLevel( which : int = -1 ) -> void :
   if in_graveyard == true:
